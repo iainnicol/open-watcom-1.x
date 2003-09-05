1d7070f2ab1fadac88fad5f2f82e7d9d049b2b27
Kendall Bennett <>
2003-09-05 19:20:29 +0100

    Updates to Open Watcom source zip batch file.
    P4:24599

--- zipup.bat
+++ zipup.bat
@@ -1,7 +1,7 @@
 @echo on
 pushd
-set LABEL=open_watcom_1.0.1
-set PREFIX=open_watcom_1.0.1
+set LABEL=open_watcom_1.1.0
+set PREFIX=open_watcom_1.1.0
 set P4OPT=-f
 set ARCHIVES=c:\archives
 cdd %ARCHIVES%
@@ -13,13 +13,6 @@ rm -rf %ARCHIVES%\ow_src
 p4 -uanonymous -Panonymous -cOPENWATCOM_SRC sync %P4OPT% @%LABEL%
 cd %ARCHIVES%\ow_src
 zip -r ..\%PREFIX%-src.zip *
-
-rem ##########################################################################
-rem ZIP all Open Watcom documentation source files
-rm -rf %ARCHIVES%\ow_docs
-p4 -uanonymous -Panonymous -cOPENWATCOM_DOCS sync %P4OPT% @%LABEL%
-cd %ARCHIVES%\ow_docs
-zip -r ..\%PREFIX%-docs.zip *
 
 popd
 
