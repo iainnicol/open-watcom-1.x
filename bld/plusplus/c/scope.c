8fee46594fd756cb3913b8569c13041a372d8b1b
Michal Necasek <>
2004-06-14 20:57:12 +0100

    Fixed build breaks caused by improperly declared functions.
    P4:26591

--- bld/plusplus/c/scope.c
+++ bld/plusplus/c/scope.c
@@ -310,7 +310,7 @@ extern SCOPE    SetCurrScope(SCOPE newScope)
     g_CurrScope = newScope;
 
 #ifndef NDEBUG
-    if( PragDbgToggle.dump_scopes ) 
+    if( PragDbgToggle.dump_scopes )
     {
         printf("Set new scope to 0x%.08X\n", newScope);
         if(newScope)
@@ -1425,8 +1425,8 @@ void ScopeJumpBackward( SCOPE scope )
     SetCurrScope(scope);
 }
 
-static scopeWalkSymbolNameSymbols( SYMBOL_NAME name, void *data )
-/***************************************************************/
+static void scopeWalkSymbolNameSymbols( SYMBOL_NAME name, void *data )
+/********************************************************************/
 {
     SYMBOL sym;
     void (*rtn)( SYMBOL ) = data;
