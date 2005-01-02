49ccc7018045d3e75f14bc14e5e481d54885e248
Hartmuth Gutsche <>
2005-01-02 20:49:18 +0100

    remove _OPT_CG
    P4:27770

--- bld/F03/cp/stmtfunc.c
+++ bld/F03/cp/stmtfunc.c
@@ -58,10 +58,6 @@ extern  bool            RecNOpn(void);
 extern  bool            RecComma(void);
 extern  bool            ReqName(int);
 
-#if _OPT_CG == _OFF
-extern  void            GSaveTemps(void);
-#endif
-
 
 void    SFPrologue() {
 //====================
@@ -74,9 +70,6 @@ void    SFPrologue() {
     sf_parm     **parm;
 
     StmtSw |= SS_SF_REFERENCED;
-#if _OPT_CG == _OFF
-    GSaveTemps();
-#endif
     CkTypeDeclared();
     SFSymId = CITNode->sym_ptr;
     if( ( SFSymId->ns.typ == TY_CHAR ) && ( SFSymId->ns.xt.size == 0 ) ) {
@@ -141,9 +134,6 @@ void    SFEpilogue() {
 
     sf_parm     *parm;
 
-#if _OPT_CG == _OFF
-    GSaveTemps();
-#endif
     GEndSF();
     parm = SFSymId->ns.si.sf.header->parm_list;
     while( parm != NULL ) {
