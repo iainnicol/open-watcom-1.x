94a31c67d9f2b2478620426a1b6db83590bfbdda
Daniel Cletheroe <>
2006-12-23 12:03:14 +0100

    add another file to put tests in for templated friends.
    P4:31147

---
+++ bld/plustest/regress/positive/source/friend3.c
@@ -0,0 +1,59 @@
+#include "fail.h"
+
+
+struct A{
+};
+
+template<class T>
+class B {
+public:
+    B( ) {}
+    B( int ii ) : i(ii) {}
+    friend A& operator<<( A&, const B<T> & );
+    friend int fnb( B<T> );
+    friend int fnb2( T );
+    friend int fnb3( int );
+private:
+    int i;
+};
+
+template<class T>
+A& operator<<( A& a, const B<T> & ) {
+    return( a );
+}
+
+template< class T >
+int fnb( B<T> b ){
+    return( b.i );
+}
+
+template< class T >
+int fnb2( T i ){
+    B<T> b(3);
+    return( b.i+i );
+}
+
+int fnb3( int i )
+{
+    B<int> b(7);
+    return( b.i+i );
+}
+
+B<char> fn( void )
+{ 
+    return( B<char>() );
+}
+
+int main()
+{
+    A out;
+    B<int> b(3);
+    
+    if( fnb( b ) != 3 ) return -1;
+    if( fnb2( 2 ) != 5 ) return -1;
+    if( fnb3( 3 ) != 10 ) return -1;
+    out << b;
+    out << fn();
+    
+    _PASS;
+}
