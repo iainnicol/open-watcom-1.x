e344cf068a0973cae44ef80e3c83eff1e2c4171e
Christof Meerwald <>
2007-03-04 00:30:14 +0100

    updated test-case to include class template destructors and operators
    P4:31322

--- bld/plustest/regress/positive/source/cltemp26.c
+++ bld/plustest/regress/positive/source/cltemp26.c
@@ -8,6 +8,12 @@ struct A
     A();
     A( const AA &a );
 
+    ~A();
+
+    bool operator ! ();
+
+    operator bool ();
+
     void f( const AA &a );
 };
 
@@ -21,6 +27,22 @@ inline A< T >::A( const AA & )
 { }
 
 template< class T >
+inline A< T >::~A()
+{ }
+
+template< class T >
+inline bool A< T >::operator ! ()
+{
+    return true;
+}
+
+template< class T >
+inline A< T >::operator bool ()
+{
+    return true;
+}
+
+template< class T >
 inline void A< T >::f( const AA & )
 { }
 
@@ -30,8 +52,15 @@ int main()
     A< int > a1;
     a1.f( a1 );
 
+    if( !( !a1 ) ) fail( __LINE__ );
+    if( !static_cast< bool >( a1 ) ) fail( __LINE__ );
+
+
     A< int > a2( a1 );
     a2.f( a2 );
+
+    if( !( !a2 ) ) fail( __LINE__ );
+    if( !static_cast< bool >( a2 ) ) fail( __LINE__ );
 
 
     _PASS;
