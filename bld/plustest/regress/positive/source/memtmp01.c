e0a3e5708477477b128f955453acbd64c6c22252
Christof Meerwald <>
2007-03-04 19:03:48 +0100

    updated test-case to include casts to references
    P4:31324

--- bld/plustest/regress/positive/source/memtmp01.c
+++ bld/plustest/regress/positive/source/memtmp01.c
@@ -52,6 +52,24 @@ int main()
     if( a.g((short) 1, (short) 2) != 3 ) fail( __LINE__ );
     if( a.h<3>() != 17 ) fail( __LINE__ );
 
+    if( static_cast< A & >( a ).f(1) != 3 ) fail( __LINE__ );
+    if( static_cast< A & >( a ).f((short) 1) != 4 ) fail( __LINE__ );
+    if( static_cast< A & >( a ).f<>(1) != 2 ) fail( __LINE__ );
+    if( static_cast< A & >( a ).f<int>(1) != 2 ) fail( __LINE__ );
+    if( static_cast< A & >( a ).f<int>((short) 1) != 2 ) fail( __LINE__ );
+    if( static_cast< A & >( a ).g((short) 1, (short) 2) != 3 ) fail( __LINE__ );
+    if( static_cast< A & >( a ).h<3>() != 17 ) fail( __LINE__ );
+
+    A &ar = a;
+
+    if( ar.f(1) != 3 ) fail( __LINE__ );
+    if( ar.f((short) 1) != 4 ) fail( __LINE__ );
+    if( ar.f<>(1) != 2 ) fail( __LINE__ );
+    if( ar.f<int>(1) != 2 ) fail( __LINE__ );
+    if( ar.f<int>((short) 1) != 2 ) fail( __LINE__ );
+    if( ar.g((short) 1, (short) 2) != 3 ) fail( __LINE__ );
+    if( ar.h<3>() != 17 ) fail( __LINE__ );
+
     B< int > b;
 
     if( b.f(1) != 3 ) fail( __LINE__ );
@@ -61,6 +79,24 @@ int main()
     if( b.f<int>((short) 1) != 2 ) fail( __LINE__ );
     if( b.g((short) 1, (short) 2) != 3 ) fail( __LINE__ );
     if( b.h<3>() != 17 ) fail( __LINE__ );
+
+    if( static_cast< B< int > & >( b ).f(1) != 3 ) fail( __LINE__ );
+    if( static_cast< B< int > & >( b ).f((short) 1) != 4 ) fail( __LINE__ );
+    if( static_cast< B< int > & >( b ).f<>(1) != 2 ) fail( __LINE__ );
+    if( static_cast< B< int > & >( b ).f<int>(1) != 2 ) fail( __LINE__ );
+    if( static_cast< B< int > & >( b ).f<int>((short) 1) != 2 ) fail( __LINE__ );
+    if( static_cast< B< int > & >( b ).g((short) 1, (short) 2) != 3 ) fail( __LINE__ );
+    if( static_cast< B< int > & >( b ).h<3>() != 17 ) fail( __LINE__ );
+
+    B< int > &br = b;
+
+    if( br.f(1) != 3 ) fail( __LINE__ );
+    if( br.f((short) 1) != 4 ) fail( __LINE__ );
+    if( br.f<>(1) != 2 ) fail( __LINE__ );
+    if( br.f<int>(1) != 2 ) fail( __LINE__ );
+    if( br.f<int>((short) 1) != 2 ) fail( __LINE__ );
+    if( br.g((short) 1, (short) 2) != 3 ) fail( __LINE__ );
+    if( br.h<3>() != 17 ) fail( __LINE__ );
 
     _PASS;
 }
