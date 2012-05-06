6640fdd54143bc807b1666d459ea0488d728ea38
Michal Necasek <>
2012-05-06 17:10:16 +0100

    Host OS specifics have no place in booths.
    P4:37299

--- boot.sh
+++ boot.sh
@@ -6,8 +6,6 @@
 # Expects POSIX or OW tools.
 
 if [ -n "$OWBOOTSTRAP" ]; then
-    export PATH=$OWBOOTSTRAP/binl:$PATH
-    export INCLUDE=$OWBOOTSTRAP/lh
     export WATCOM=$OWBOOTSTRAP
 fi
 cd $OWROOT/src/make
@@ -26,8 +24,6 @@ cd $OWROOT/src
 builder boot 
 RC=$?
 if [ -n "$OWBOOTSTRAP" ]; then
-    export PATH=$OWBINDIR:$OWROOT/build:$OWDEFPATH
-    export INCLUDE=$OWDEFINCLUDE
     export WATCOM=$OWDEFWATCOM
 fi
 if [ $RC -eq 0 ]; then
