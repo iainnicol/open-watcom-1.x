0e3fcb2e5f2b3d18b7ff573c2d86c4d30612a674
Michal Necasek <>
2012-11-16 16:02:12 +0100

    Updated V2 bootstrap batch file.
    P4:37482

--- boot.bat
+++ boot.bat
@@ -1,38 +1,47 @@
 @echo off
 rem Batch to build a subset of the Open Watcom tools
 
+rem Set up environment variables needed by Open Watcom (if provided)
 if "%OWBOOTSTRAP%" == "" goto cont1
 set WATCOM=%OWBOOTSTRAP%
 set PATH=%OWBOOTSTRAP_PATH%
 set INCLUDE=%OWBOOTSTRAP_INCLUDE%
 :cont1
+
+rem Build wmake
 cd %OWROOT%\src\make
 if not exist %OWOBJDIR% mkdir %OWOBJDIR%
 cd %OWOBJDIR%
-if "%OWBOOTSTRAP%" == "" goto nowatcom
-wmake -h -f ..\wmake
-goto cont2
-:nowatcom
 nmake -nologo -f ..\nmkmake
-:cont2
+
+rem Build the builder utility and rm
 cd %OWROOT%\src\builder
 if not exist %OWOBJDIR% mkdir %OWOBJDIR%
 cd %OWOBJDIR%
 %OWBINDIR%\wmake -h -f ..\bootmake builder.exe rm.exe
 cd %OWROOT%\src
+
+rem With wmake and builder at hand, run the bootstrap build
 builder boot
-if errorlevel == 1 goto cont4
+if errorlevel == 1 goto restore_vars
+
+rem Modify the environment to point at freshly built tools
 if "%OWBOOTSTRAP%" == "" goto cont3
-set PATH=%OWBINDIR%;%OWROOT%\build;%OWDEFPATH%
-set INCLUDE=%OWDEFINCLUDE%
-set WATCOM=%OWDEFWATCOM%
+rem set PATH=%OWBINDIR%;%OWROOT%\build;%OWDEFPATH%
+rem set INCLUDE=%OWDEFINCLUDE%
+rem set WATCOM=%OWDEFWATCOM%
 :cont3
+
+rem Do a regular build using fresh tools
 builder build
 goto finish
-:cont4
+
+rem Restore original values of environment variables
+:restore_vars
 if "%OWBOOTSTRAP%" == "" goto finish
 set PATH=%OWBINDIR%;%OWROOT%\build;%OWDEFPATH%
 set INCLUDE=%OWDEFINCLUDE%
 set WATCOM=%OWDEFWATCOM%
+
 :finish
 cd %OWROOT%
